ffe14393287dcfc0bfa23316118e86a834051cc3
tittua <tittua1997@gmail.com>
2024-04-27 19:39:31 +0530

    Second commit

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-import flask as Flask
+from flask import Flask
 
 app=Flask(__name__)
 
+
+#decorator
 @app.route('/')
 def home():
     return 'Hello Welcome to home page'
 
+#variable rule
+@app.route('/success/<int:score>')
+def success(score):
+    return 'The person has passed '+ str(score)
+
+#variable rule
+@app.route('/fail/<int:score>')
+def fail(score):
+    return 'The person has failed '+ str(score)
+
 
 if __name__=='__main__':
-    app.run()
+    app.run(debug=True)
